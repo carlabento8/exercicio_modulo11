8c47fbfc8af28a60325fef909c2bd6004081c266
Carla Bento <carla.bento@0utlook.com>
2021-08-23 22:34:18 -0300

    lista de produtos

---
+++ cypress/integration/produto.spec.js
@@ -0,0 +1,37 @@
+/// <reference  types="cypress" />
+
+
+describe('Funcionalidade página de produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Selecionar produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last
+            //.eq(3)
+            .contains('Ariel Roll Sleeve Sweatshirt')
+            .click()
+    });
+
+    it.only('Adicionar um produto ao carrinho', () => {
+        var quantidade = 5
+
+
+        cy.get('[class="product-block grid"]')
+        .contains('Ariel Roll Sleeve Sweatshirt').click()
+        cy.get('.button-variable-item-Green').click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+    
+       
+    
+    });
+
+});
